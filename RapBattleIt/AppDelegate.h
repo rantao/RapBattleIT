ced0346bc10449c4790902f4a9174f92f641bd5e
Ran Tao <ran@ephexi.com>
2012-10-17 20:14:35 -0700

    Initial Commit

---
+++ RapBattleIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RapBattleIt
+//
+//  Created by Ran Tao on 10.17.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
